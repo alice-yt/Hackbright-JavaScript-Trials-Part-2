8bcd1d0c8eb1d009a547cdf5d07340e5ea467eec
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-21 00:02:26 +0000

    defined everyOtherItem function

--- arrays.js
+++ arrays.js
@@ -9,13 +9,18 @@ function printIndices(items) {
 }
 
 // 2. everyOtherItem
-// function everyOtherItem(items) {
-//   const result = []
+function everyOtherItem(items) {
+  const result = []
 
-//   for (const item in items)
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i])
+    }
+  }
+  return result;
+}
 
-// }
-
+// everyOtherItem(['apple', 'berry', 'cherry', 'mango', 'strawberry']);
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
